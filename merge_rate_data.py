4c4b0145ad2785a24e7ed47f9af71f824da041eb
Raymond2Moran <124376400+Raymond2Moran@users.noreply.github.com>
2023-02-04 15:00:43 -0500

    Update merge_rate_data.py
    Joint by taxon and taxon order. Use outer join instead of inner to select the union of taxa and order instead of intersections.
    
    The reason of doing this is that when I used inner join for the whole RTL9 protein, the number of shared taxa equals zero. i.e., none of them have a amino acid substitution rate at every amino acid position. So we have to use outer join.

--- merge_rate_data.py
+++ merge_rate_data.py
@@ -25,7 +25,11 @@ for i in len(files): #for each csv in unmerged data list
   if i == 0:
     new_df = files[i] #save first file as new df
   if i > 0:
-    new_df = pd.merge(new_df, file[i], on = "taxon", how = "inner") #merge first file with next file in list by taxa, then merge merged file with next in taxa
+    new_df = pd.merge(new_df, file[i], on = ["taxon","taxon order"], how = "outer") 
+    #Merge first file with next file in list by taxa and taxon order.
+    #Use outer join so that files are joined by union of taxa and order, because not every aa position
+    #have the same group of taxa.
+    #Taxa and taxa order only appears one time at the start of the mastersheet.
     
 new_df.to_csv(protein+".csv")
     
